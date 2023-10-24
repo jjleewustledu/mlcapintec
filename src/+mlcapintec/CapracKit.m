bf7050450160c1b24e5dcd7a8e6effae7cbc15aa
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2023-10-23 23:19:23 -0500

    do_make_device(this)

--- src/+mlcapintec/CapracKit.m
+++ src/+mlcapintec/CapracKit.m
@@ -7,23 +7,49 @@ classdef (Sealed) CapracKit < handle & mlkinetics.InputFuncKit
     
     methods
         function ic = do_make_activity(this, varargin)
-            do_make_device(this);
-            a = this.device_.activity(varargin{:});
-            ic = this.buildImagingContext(a);
+            if isempty(this.device_)
+                do_make_device(this);
+            end
+            a = this.device_.activity();
+            ic = this.do_make_input_func(a);
         end
         function ic = do_make_activity_density(this, varargin)
-            do_make_device(this);
-            a = this.device_.activityDensity(varargin{:});
-            ic = this.buildImagingContext(a);
+            if isempty(this.device_)
+                do_make_device(this);
+            end
+            a = this.device_.activityDensity();
+            ic = this.do_make_input_func(a);
         end
         function ic = do_make_activity_density_interp1(this, varargin)
-            do_make_device(this);
-            a = this.device_.activityDensityInterp1(varargin{:});
-            ic = this.buildImagingContext(a);
+            if isempty(this.device_)
+                do_make_device(this);
+            end
+            a = this.device_.activityDensityInterp1();
+            ic = this.do_make_input_func(a);
         end
         function dev = do_make_device(this, varargin)
             this.device_ = this.buildCountingDevice(varargin{:});
             dev = this.device_;
+        end
+        function ic = do_make_input_func(this, measurement)
+            arguments
+                this mlcapintec.CapracKit
+                measurement {mustBeNumeric,mustBeNonempty}
+            end
+
+            if isempty(this.device_)
+                do_make_device(this);
+            end
+            med = this.bids_kit_.make_bids_med();
+            ifc = copy(med.imagingFormat);
+            ifc.img = measurement;
+            ifc.fileprefix = sprintf("%s_%s", ifc.fileprefix, stackstr(3));
+            ifc.json_metadata.taus = this.device_.taus;
+            ifc.json_metadata.times = this.device_.times;
+            ifc.json_metadata.timesMid = this.device_.timesMid;
+            ifc.json_metadata.timeUnit = "second";
+            this.input_func_ic_ = mlfourd.ImagingContext2(ifc);
+            ic = this.input_func_ic_;
         end
     end
 
@@ -66,23 +92,6 @@ classdef (Sealed) CapracKit < handle & mlkinetics.InputFuncKit
                 input_func_dev = this.scanner_kit_.alignArterialToScanner( ...
                     input_func_dev, scanner_dev, 'sameWorldline', opts.sameWorldline);
             end
-        end
-        function ic = buildImagingContext(this, measurement)
-            arguments
-                this mlcapintec.CapracKit
-                measurement {mustBeNumeric,mustBeNonempty}
-            end
-
-            med = this.bids_kit_.make_bids_med();
-            ifc = copy(med.imagingFormat);
-            ifc.img = measurement;
-            ifc.fileprefix = sprintf("%s_%s", ifc.fileprefix, stackstr(3));
-            ifc.json_metadata.taus = this.device_.taus;
-            ifc.json_metadata.times = this.device_.times;
-            ifc.json_metadata.timesMid = this.device_.timesMid;
-            ifc.json_metadata.timeUnit = "second";
-            this.input_func_ic_ = mlfourd.ImagingContext2(ifc);
-            ic = this.input_func_ic_;
         end
         function this = CapracKit()
         end
